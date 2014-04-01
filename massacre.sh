48acc87630dc36dba520984ea5f48bde957b3b3e
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-01 13:24:00 -0700

    Release!

--- massacre.sh
+++ massacre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Well, okay. I guess you wanted to run this. It's not done yet."
+echo "Well, okay. I guess you wanted to run this. You probably should run shell code from the internet."
