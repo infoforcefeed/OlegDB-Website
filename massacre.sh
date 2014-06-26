37a8b9d3457b76b393188d6b0fa2679d63b65e2b
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-06-26 11:53:11 -0700

    Updated massacre.sh.

--- massacre.sh
+++ massacre.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "Well, okay. I guess you wanted to run this. You probably should run shell code from the internet."
+echo "Well, okay. I guess you wanted to run this. You probably shouldn't run shell code from the internet."
+echo "If you really want to build/run/try OlegDB, check out and build from source: https://github.com/infoforcefeed/OlegDB/archive/v.0.1.2.tar.gz"
