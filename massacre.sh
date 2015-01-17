0518a4ec402841117eb8e061583acf3865c69d1f
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-01-17 09:44:19 -0800

    Update massacre.sh, Oleg commit.

--- massacre.sh
+++ massacre.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "Well, okay. I guess you wanted to run this. You probably shouldn't run shell code from the internet."
-echo "If you really want to build/run/try OlegDB, check out and build from source: https://github.com/infoforcefeed/OlegDB/archive/v.0.1.2.tar.gz"
+echo "If you really want to build/run/try OlegDB, check out and build from source:"
+echo "https://github.com/infoforcefeed/OlegDB/releases"
+echo "XOXO"
